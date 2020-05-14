68272fef6a4805a69a043a6d3ce314b9bc403469
abmasadullah <abmasadullah@gmail.com>
2020-05-14 11:12:38 +0600

    base updated

--- qabangla/qabangla/urls.py
+++ qabangla/qabangla/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+	path('', include('base.urls')),
+	path('qa/', include('qa.urls')),
     path('admin/', admin.site.urls),
 ]

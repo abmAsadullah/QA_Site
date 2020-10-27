5e58d8bf4e411fb16c4f0a781e1fc226e5f59413
abmasadullah <abmasadullah@gmail.com>
2020-10-27 15:07:50 +0600

    Removed Django for testing

--- qabangla/qa/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Question
-
-
-def qa(request):
-	cont = {
-		'questions': Question.objects.all()
-	}
-	return render(request, 'qa/qapage.html', cont)
